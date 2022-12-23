c92b822298e6371689b577764cc8645c93d0abe6
Greg Sarjeant <gsarjeant@gmail.com>
2022-12-22 22:14:50 -0500

    Reimplement SiteMenu with MUI components
    I'm trying out moving over to MUI components for the majority of the UI.
    I don't need to be implementing a full suite of custom components, and I thought this might save some time.
    
    That said, so far it hasn't. I think the horizontal menu isn't really something MUI supports.
    I can have an AppBar with buttons, and I can pretty easily implememt a vertical menu that shows the active
    item. But doing this as a horizontal menu stil ended up being a bunch of messing with divs.
    
    That said, hopefully I benefit some from the standard typography components and whatnot.
    I also need ot see how much more of this I can implement as theme overrides so all the color definitions
    are in one place.
    
    But for now, this commit has the menu bar looking and acting as it did before using all 3rd-party components.

--- web/components/siteMenu.js
+++ web/components/siteMenu.js
@@ -1,33 +1,41 @@
 import Link from 'next/link';
-import { MarkGithubIcon } from '@primer/octicons-react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
 import Tooltip from '@mui/material/Tooltip';
-import styles from './siteMenu.module.css';
+import { Typography } from '@mui/material';
+import { MarkGithubIcon } from '@primer/octicons-react'
 import { getCurrentSection } from '../lib/utils';
 import { siteSections } from "../lib/settings";
+import styles from './siteMenu.module.css';
 
 export default function SiteMenu() {
     return (
-        <>
-            <ul className={styles.menuList}>
-                {siteSections.map((section) => (
-                    <li key={section.name} className={section.href === `/${getCurrentSection()}` ? styles.menuItemActive : styles.menuItem}>
-                        {/* If I ever need this elsewhere, I'll pull it out into a separate component, but it's fine here for now. */}
-                        <Link className={section.href === `/${getCurrentSection()}` ? styles.menuLinkActive : styles.menuLink} href={`${section.href}`}>
-                            {section.name}
-                        </Link>
-                    </li>
-
-                ))}
-
-                {/* I'll get rid of this hardcoding as soon as I have more than one of these. */}
-                <li key="github" className={styles.menuItemRight}>
-                    <Tooltip title="view source code for this site">
+        <AppBar position="static" elevation={0}>
+            <Toolbar variant="dense">
+                <Box display="flex" flexGrow={1}>
+                    {siteSections.map((section) => {
+                        return section.href === `/${getCurrentSection()}`
+                            ?
+                            <Typography component="div" className={styles.menuItemActive}>
+                                {section.name}
+                            </Typography>
+                            :
+                            <Typography component="div" className={styles.menuItem}>
+                                <Link className={styles.menuLink} href={`${section.href}`}>
+                                    {section.name}
+                                </Link>
+                            </Typography>
+                    })}
+                </Box>
+                <Tooltip title="view source code for this site">
+                    <Typography component="div" className={styles.menuItem}>
                         <Link className={styles.menuLink} href="https://www.github.com/gsarjeant/gregsarjeant.net">
                             <MarkGithubIcon verticalAlign="middle" size={24} />
                         </Link>
-                    </Tooltip>
-                </li>
-            </ul>
-        </>
-    )
+                    </Typography>
+                </Tooltip>
+            </Toolbar>
+        </AppBar >
+    );
 }
